b5ade3020efda546c3c2c7007f5c909bf79645a3
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-01-21 23:38:52 +0100

    Update przelewanka.ml

--- przelewanka.ml
+++ przelewanka.ml
@@ -10,7 +10,7 @@ let przelewanka a =
       begin
         Array.iter (fun (x, y) -> if y = 0 || x = 0 then raise Dobrze else ()) a;
         false;
-      end
+      end;
     with Dobrze ->
     let rec nwd a b =
       if b = 0 then a else nwd b (a mod b) in
@@ -20,7 +20,7 @@ let przelewanka a =
       begin
         Array.iter (fun (_, y) -> if y mod nwd_pojemnosci <> 0 then raise Zle else ()) a;
         true;
-      end
+      end;
     with Zle -> false;
   if not warunek_konieczny then -1 else
   let wynik = ref (-1) in
@@ -39,6 +39,29 @@ let przelewanka a =
   begin
     dodaj (start, 0);
     while (not Queue.is_empty q) && !wynik = -1 do
-      let (s, r) = Queue.take 
+      let (s, r) = Queue.take in
+        for i = 0 to n - 1 do
+          let ns = Array.copy s in
+          ns.(i) <- fst a.(i);
+          dodaj (ns, r + 1);
+          ns.(i) <- 0;
+          dodaj (ns, r + 1);
+          ns.(i) <- s.(i)
+            for j = 0 to n - 1 do
+              if i <> j then
+                let ile = min (fst a.(j) - s.(j)) s.(i) in
+                ns.(i) <- ns.(i) - ile;
+                ns.(j) <- ns.(j) + ile;
+                dodaj ns;
+                ns.(i) <- s.(i);
+                ns.(j) <- s.(j);
+            done;
+        done;
+    done;
+  end;
+  !wynik
+          
+              
+          
   
       
