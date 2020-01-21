44f8d2d841b56ec9e68b7c307ae22958ed992fdf
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-01-22 00:21:56 +0100

    Update przelewanka666.ml

--- przelewanka666.ml
+++ przelewanka666.ml
@@ -1,23 +1,23 @@
 let przelewanka a =
-  let n = Array.lenght a in
+  let n = Array.length a in
   if n = 0 then 0 else
   let warunek_konieczny =
     try
       begin
-        Array.iter (fun (x, y) -> if y = 0 || x = 0 then raise Dobrze else ()) a;
+        Array.iter (fun (x, y) -> if y = 0 || x = 0 then raise Exit else ()) a;
         false;
       end;
-    with Dobrze ->
+    with Exit ->
     let rec nwd a b =
       if b = 0 then a else nwd b (a mod b) in
     let nwd_pojemnosci =
       Array.fold_left (fun acc (x, _) -> nwd x acc) 0 a in
     try
       begin
-        Array.iter (fun (_, y) -> if y mod nwd_pojemnosci <> 0 then raise Zle else ()) a;
+        Array.iter (fun (_, y) -> if y mod nwd_pojemnosci <> 0 then raise Exit else ()) a;
         true;
       end;
-    with Zle -> false;
+    with Exit -> false; in
   if not warunek_konieczny then -1 else
   let wynik = ref (-1) in
   let q = Queue.create () in
@@ -25,17 +25,17 @@ let przelewanka a =
   let start = Array.make n 0 in
   let koniec = Array.init n (fun i -> snd a.(i)) in
   let dodaj (stan, ruchy) =
-    if not Hashtbl.mem h stan then (
+    if not (Hashtbl.mem h stan) then (
       if stan = koniec then (
         wynik := ruchy;
         Queue.clear q;)
-      else
-      Hashtbl.add h (stan ruchy);
-      Queue.add (stan ruchy) q;) in
+      else (
+      Hashtbl.add (Array.copy stan) h;
+      Queue.add (stan, ruchy) q;)) in
   begin
     dodaj (start, 0);
-    while (not Queue.is_empty q) && !wynik = -1 do
-      let (s, r) = Queue.take in
+    while not (Queue.is_empty q) && !wynik = -1 do
+      let (s, r) = Queue.take q in
         for i = 0 to n - 1 do
           let ns = Array.copy s in
           ns.(i) <- fst a.(i);
