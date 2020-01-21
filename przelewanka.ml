8af936e303a08a2f25fc0cc307ceb5d60ac3e892
qwdpokqwdpokqwdpok <56348150+qwdpokqwdpokqwdpok@users.noreply.github.com>
2020-01-21 21:08:27 +0100

    Update przelewanka.ml

--- przelewanka.ml
+++ przelewanka.ml
@@ -1,1 +1,23 @@
 
+
+
+
+let przelewanka a =
+  let warunek_konieczny =
+    try
+      begin
+        Array.iter (fun (x, y) -> if y = 0 || x = 0 then raise Dobrze else ()) a;
+        false
+      end
+    with Dobrze ->
+    let rec nwd a b =
+      if b = 0 then a else nwd b (a mod b) in
+    let nwd_pojemnosci =
+      Array.fold_left (fun acc (x, _) -> nwd x acc) 0 a in
+    try
+      begin
+        Array.iter (fun (_, y) -> if y mod nwd_pojemnosci <> 0 then raise Zle else ()) a;
+        true
+      end
+    with Zle -> false
+
